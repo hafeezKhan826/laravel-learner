6f77bce1ec7951eb50cb48f6e044d8189e6f74b0
Sonam <sonam@zool.in>
2018-07-23 12:23:45 +0530

    ÜnCommenting a sample route

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,13 @@ Route::get('/', function () {
 Route::get('/', 'PagesController@index');
 
 Route::get('/about', 'AboutController@about');
-/* 
+
 Route::get('/hello', function () {
     return 'Hello';
 });
- */
+
 
 Route::get('/user/{id}/{name}', function ($id,$name) {
     return 'user is: ' . $id;
 });
+
